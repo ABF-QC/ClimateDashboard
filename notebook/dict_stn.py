9ab24cea47f9c2330c5a267d262685cbef1858dd
Anna-Belle Filion <treblecharger34@hotmail.com>
2025-02-16 10:21:11 -0500

    added module 1 project section

---
+++ notebook/dict_stn.py
@@ -0,0 +1,34 @@
+stn_reports = {
+    'hly_dly_mly' : {
+        'name' : 'Hourly & Daily & Montlhy',
+        'color' : 'purple',
+    },
+    'hly_dly' : {
+        'name' : 'Hourly & Daily',
+        'color' : 'blue',
+    },
+    'hly_mly' : {
+        'name' : 'Hourly & Monthly',
+        'color' : 'black',
+    },
+    'hly' : {
+        'name' : 'Hourly',
+        'color' : 'red',
+    },
+    'dly_mly' : {
+        'name' : 'Daily & Montlhy',
+        'color' : 'orange',
+    },
+    'dly' : {
+        'name' : 'Daily',
+        'color' : 'lightblue',
+    },
+    'mly' : {
+        'name' : 'Monthly',
+        'color' : 'yellow',
+    },
+    'other' : {
+        'name' : 'No reports',
+        'color' : 'pink',
+    },
+}
